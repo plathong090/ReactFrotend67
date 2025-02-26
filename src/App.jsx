6ada81cdc5fac8296c03237fde4f60689829d740
plathong090 <prangthong250947gmail.com>
2025-02-26 10:38:04 +0700

    testw/example

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,154 @@
-import Contact from './assets/Contact.jsx';
-import Hello from './assets/hello.jsx';
-import Counter from './assets/Counter.jsx';
+// import Contact from './assets/Contact.jsx';
+// import Hello from './assets/hello.jsx';
+// import Counter from './assets/Counter.jsx';
 
-function App() {
-  const helloData = [
-    { name: 'Biw', message: 'Hi there!' },
-    { name: 'Nino', message: 'Hello' }
-  ];
+// function App() {
+//   const helloData = [
+//     { name: 'Biw', message: 'Hi there!' },
+//     { name: 'Nino', message: 'Hello' }
+//   ];
+
+//   return (
+//     <>
+//       <div>
+//         {helloData.map((data, index) => (
+//           <Hello key={index} name={data.name} message={data.message} />
+//         ))}
+
+//         <center>
+//           <Contact email="test123@gmail.com" phone="0901234567" />
+//         </center>
+
+//         {/* เพิ่ม Counter.jsx ลงไป */}
+//         <center><Counter /></center>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import { useState } from 'react';
+
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
 
   return (
-    <>
-      <div>
-        {helloData.map((data, index) => (
-          <Hello key={index} name={data.name} message={data.message} />
-        ))}
-
-        <center>
-          <Contact email="test123@gmail.com" phone="0901234567" />
-        </center>
-
-        {/* เพิ่ม Counter.jsx ลงไป */}
-        <center><Counter /></center>
-      </div>
-    </>
+    <div>
+      <SearchBar 
+        filterText={filterText} 
+        inStockOnly={inStockOnly} 
+        onFilterTextChange={setFilterText} 
+        onInStockOnlyChange={setInStockOnly} />
+      <ProductTable 
+        products={products} 
+        filterText={filterText}
+        inStockOnly={inStockOnly} />
+    </div>
   );
 }
 
-export default App;
+function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
+    </tr>
+  );
+}
+
+function ProductRow({ product }) {
+  const name = product.stocked ? product.name :
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+function ProductTable({ products, filterText, inStockOnly }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (
+      product.name.toLowerCase().indexOf(
+        filterText.toLowerCase()
+      ) === -1
+    ) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category} />
+      );
+    }
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name} />
+    );
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange
+}) {
+  return (
+    <form>
+      <input 
+        type="text" 
+        value={filterText} placeholder="Search..." 
+        onChange={(e) => onFilterTextChange(e.target.value)} />
+      <label>
+        <input 
+          type="checkbox" 
+          checked={inStockOnly} 
+          onChange={(e) => onInStockOnlyChange(e.target.checked)} />
+        {' '}
+        Only show products in stock
+      </label>
+    </form>
+  );
+}
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+export default function App() {
+  return <FilterableProductTable products={PRODUCTS} />;
+}
